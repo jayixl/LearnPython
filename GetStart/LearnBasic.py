fd61a4425df6eec9f4da308c0acabdb0847564fd
jayixl <jay.jin@live.com>
2016-06-29 22:46:20 +0800

    add basic

---
+++ GetStart/LearnBasic.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 29 22:40:45 2016
+
+@author: jayjin
+"""
+
+str = 'this is a string object'
+
+print str.capitalize()
+
+print str.split()
+
+print str.replace(' ', '_')
+
+print str.find('string')
+
