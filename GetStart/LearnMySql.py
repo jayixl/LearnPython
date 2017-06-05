64a196ef17808d337d1c8f5cf3271647a7315c25
Jie Jin <jie.jin@envisioncn.com>
2017-06-05 10:16:15 +0800

    refine

--- GetStart/LearnMySql.py
+++ GetStart/LearnMySql.py
@@ -14,7 +14,7 @@ sys.path.insert(1, '../Stock')
 import MySqlUtils
 
 
-def SingleStockRegDataGet(df, factors, end_date, start_offset, shift = 20):
+def SingleStockRegDataGet(df, factors, end_date, start_offset, shift=20):
     """ 
     get factor data from original df
         
@@ -31,26 +31,26 @@ def SingleStockRegDataGet(df, factors, end_date, start_offset, shift = 20):
     shift : integer
         offset of days to calculate forward price    
     """
-    
+
     # maybe we cant find end_date, locate the nearest earlier date
     # end_date = df['date'][df['date'] <= end_date][-1] # equivalent way
     end_date = df.index[df.index <= end_date][-1]
-    
+
     end_loc = df.index.get_loc(end_date)
-    start_loc = max(end_loc - start_offset, 0)   
-    
+    start_loc = max(end_loc - start_offset, 0)
+
     # here we are using mixed integer and label based access, only .ix supports
-    df = df.ix[start_loc:end_loc, factors] 
-    
+    df = df.ix[start_loc:end_loc, factors]
+
     df['fwdPrice'] = df['tclose'].shift(-shift)
     df = df[:-shift]
-    
+
     # calculate the ratio of rows that contain null
     criteria_any_null = df.isnull().any(1)
     ratio = df[criteria_any_null].shape[0] * 1.0 / criteria_any_null.count()
     if ratio > 0.2:
         return None
-    
+
     # as we need to do interpolate() for missing values,
     # we should guarantee that the first and last row contain no null
     # give each one a chance '''
@@ -58,49 +58,48 @@ def SingleStockRegDataGet(df, factors, end_date, start_offset, shift = 20):
         df = df.ix[1:]
     if df.ix[-1].isnull().any():
         df = df.ix[0:-1]
-    
+
     if df.ix[0].isnull().any() or df.ix[-1].isnull().any():
-        return None    
-    
-    # remove invalid rows (i.e., the rows containing null)
-    criteria_no_null = df.notnull().all(1) # all(1) indicates apply all() on rows
-    df = df[criteria_no_null] # criteria is a boolean Series
-    
+        return None
+
+        # remove invalid rows (i.e., the rows containing null)
+    criteria_no_null = df.notnull().all(1)  # all(1) indicates apply all() on rows
+    df = df[criteria_no_null]  # criteria is a boolean Series
+
     return df
 
+
 def StockRegDataGet(universe, training_days):
-        
     '''
     training_days must be confirmed to be existing days, but any one could be filtered if it is invalid
     '''
-    
+
     data_all = {}
-    
+
     for symbol in universe:
-        print symbol       
-        
-    
+        print symbol
+
     return None
-    
+
 
 query_res = MySqlUtils.mysql_histrade_query('000001', '1995-01-01', '1995-03-03')
 print 'get %d rows from db' % len(query_res)
 
 # type(query_res) is tuple tuple
 data = [[col for col in row] for row in query_res]
-df = pd.DataFrame(data, index = [t[1] for t in data], columns = MySqlUtils.db_columns)
+df = pd.DataFrame(data, index=[t[1] for t in data], columns=MySqlUtils.db_columns)
 
 # we can't get column name info by lambda
-#df = df.apply(lambda col: pd.to_numeric(col, errors='coerce'))
-#df = df.apply(lambda col: col.astype('float'))
+# df = df.apply(lambda col: pd.to_numeric(col, errors='coerce'))
+# df = df.apply(lambda col: col.astype('float'))
 for col_name in df.columns:
     if (col_name != 'symbol' and col_name != 'date' and col_name != 'name'):
-        df[col_name] = pd.to_numeric(df[col_name], errors='coerce') 
+        df[col_name] = pd.to_numeric(df[col_name], errors='coerce')
         df[col_name] = df[col_name].interpolate()
 
 factors = ['tclose', 'ma10']
 df2 = SingleStockRegDataGet(df, factors, '1995-02-29', 10, 5)
 print df2
 
-#print string.join(MySqlUtils.db_columns, ',')
-#print ','.join(["'%s'" % col for col in MySqlUtils.db_columns])
+# print string.join(MySqlUtils.db_columns, ',')
+# print ','.join(["'%s'" % col for col in MySqlUtils.db_columns])
--- MySql/MySqlUtils.py
+++ MySql/MySqlUtils.py
@@ -6,54 +6,64 @@ Created on Sun Nov 08 13:23:32 2015
 """
 import MySQLdb
 import os
+import pandas as pd
 
 _host = 'localhost'
 _user = 'root'
-_password = '1234'
-db = 'netease_stock'
-db_columns = ['symbol', 'date', 'name', 'topen', 'tclose', 'high', 'low', 
-              'lclose', 'chg', 'pchg', 'turnover', 'voturnover', 'vaturnover', 
-              'tcap', 'mcap', 'ma5', 'ma10', 'ma20', 'ma30', 'ma60']
- 
+_password = '123456'
+db = 'trade'
+histrade_columns = ['symbol', 'date', 'name', 'topen', 'tclose', 'high', 'low', 'lclose', 'chg', 'pchg', 'turnover',
+                    'voturnover', 'vaturnover', 'tcap', 'mcap', 'ma5', 'ma10', 'ma20', 'ma30', 'ma60']
+
+fundflow_columns = ['symbol', 'date', 'tclose', 'pchg', 'turnover', 'inflow', 'outflow', 'netinflow', 'maininflow',
+                    'mainoutflow', 'mainnetinflow']
+
 
 def mysql_test():
-        
     try:
-        conn = MySQLdb.connect(host=_host,user=_user,passwd=_password,db=db,port=3306)
+        conn = MySQLdb.connect(host=_host, user=_user, passwd=_password, db=db, port=3306, charset='utf8')
         cur = conn.cursor()
-        cur.execute('select %s from histrade_day' % (','.join(db_columns), ))
-        top1 = cur.fetchone()
-        print top1
-        
-    except MySQLdb.Error,e:
+        cur.execute('select * from codelist')
+        res = cur.fetchall()
+        data = [[col for col in row] for row in res]
+        print data
+        df = pd.DataFrame(data)
+        df.to_csv("e:/test/out.csv", encoding='utf-8')
+        print df
+
+
+        # f = open('e:/test/out.txt', 'w')
+        # f.write("".join(top1.))
+        # f.close()
+
+    except MySQLdb.Error, e:
         print "Mysql Error %d: %s" % (e.args[0], e.args[1])
-        
+
     finally:
         cur.close()
         conn.close()
 
+
 def mysql_generic_query(query):
-    
     '''
     generic query
     '''
     try:
-        conn = MySQLdb.connect(host=_host,user=_user,passwd=_password,db=db,port=3306)
+        conn = MySQLdb.connect(host=_host, user=_user, passwd=_password, db=db, port=3306, charset='utf8')
         cur = conn.cursor()
         cur.execute(query)
         res = cur.fetchall()
         return res
-        
-    except MySQLdb.Error,e:
+
+    except MySQLdb.Error, e:
         print "Mysql Error %d: %s" % (e.args[0], e.args[1])
-        
+
     finally:
         cur.close()
         conn.close()
 
 
 def mysql_histrade_query(symbol, start, end):
-    
     """ query history trades with symbol, start date, end date
         
     Parameters
@@ -64,25 +74,56 @@ def mysql_histrade_query(symbol, start, end):
         start date, format 'yyyy-MM-dd'
     end : string
         end date, format 'yyyy-MM-dd'
-    """        
-    
+    """
+
     query = '''SELECT %s FROM histrade_day
-        WHERE symbol = '%s' AND date >= '%s' AND date <= '%s' ''' % (','.join(db_columns), symbol, start, end)
-                
+        WHERE symbol = '%s' AND date >= '%s' AND date <= '%s' ''' % (','.join(histrade_columns), symbol, start, end)
+
     try:
-        conn = MySQLdb.connect(host=_host, user=_user, passwd=_password, db=db, port=3306)
+        conn = MySQLdb.connect(host=_host, user=_user, passwd=_password, db=db, port=3306, charset='utf8')
         cur = conn.cursor()
         cur.execute(query)
-        #print 'there are %s rows record' % count
+        # print 'there are %s rows record' % count
         res = cur.fetchall()
         return res
-        
-    except MySQLdb.Error,e:
+
+    except MySQLdb.Error, e:
         print "Mysql Error %d: %s" % (e.args[0], e.args[1])
-        
+
     finally:
         cur.close()
         conn.close()
-        
-    return None       
-    
+
+    return None
+
+
+def fund_flow_query(symbol, start, end):
+    """
+    :param symbol:
+    :param start:
+    :param end:
+    :return:
+    """
+
+    query = '''SELECT %s FROM fund_flows
+        WHERE symbol = '%s' AND date >= '%s' AND date <= '%s' ''' % (','.join(fundflow_columns), symbol, start, end)
+
+    try:
+        conn = MySQLdb.connect(host=_host, user=_user, passwd=_password, db=db, port=3306, charset='utf8')
+        cur = conn.cursor()
+        cur.execute(query)
+        # print 'there are %s rows record' % count
+        res = cur.fetchall()
+        return res
+
+    except MySQLdb.Error, e:
+        print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+
+    finally:
+        cur.close()
+        conn.close()
+
+    return None
+
+
+mysql_test()
